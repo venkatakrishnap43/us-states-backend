6c21b6dd5e9821e91fa8ad08434179bea4d8906a
venkatakrishnap43 <venkata.krishnap43@gmail.com>
2021-04-24 19:56:20 -0500

    Intial

---
+++ src/main/java/com/usastates/usastates/UsaStatesApplication.java
@@ -0,0 +1,13 @@
+package com.usastates.usastates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsaStatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsaStatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usastates/usastates/UsaStatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usastates.usastates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsaStatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
